b4f715f56c76562c0c480182e9acfe509fc26b7b
Javier Olivas Blanco <144839350+JavieRetro@users.noreply.github.com>
2026-01-23 18:14:10 +0100

    Cambios en tableros, variables y nuevas funciones.
    Main.py:  el tablero visible, he añadido el emoticono oculto, en el final del juego al hundir los barcos de la flota en el tablero visible, se muestra en este todo el tablero al completo.
    
    Funciones_flota.py: Borramos la función de crear tablero, la tenía ya introducida en el main así que sobraba como tal. Modifique las distancias entre las separaciones laterales del tablero. Las funciones de barco hundido que recorriendo el tablero donde todas las cuadriculas de los barcos pasen de estar tocadas, a hundidas.  Y la función al quedar barcos recorriendo el tablero si cualquiera de la letras de la flota sigue dentro de el retornamos True, si no se sigue.

--- main.py
+++ main.py
@@ -1,46 +1,55 @@
 from funciones_flota import(
     flota,
     Agua,
+    Hundido,
+    Oculto,
     mostrarTablero,
     colocar_barcos,
     comprobar_barcos_hundidos,
     disparar,
-    faltan_barcos
+    quedan_barcos
 )
 
 print("🌊🚢 HUNDIR LA FLOTA 🚢🌊")
 
-#Tamaño para el tablero
+#Pedimos el tamaño para el tablero
 
 tam = int(input("Introduce un tamaño para el tablero (8 o 10): "))
 
+#Validación del tamaño si es correcto o no
 while tam not in [8, 10]:
     tam = int(input("Tamaño inválido, introduce otro(8 o 10): "))
 
 #Creacion de tableros
-tablero_visible = [[Agua for _ in range(tam)] for _ in range(tam)]
+#El tablero visible es el que ve el jugador mientras juega, formado por piezas ⬜
+tablero_visible = [[Oculto for _ in range(tam)] for _ in range(tam)]
+
+#El tablero oculto contiene la información del juego
+#Tanto el agua como los barcos
 tablero_oculto = [[" " for _ in range(tam)] for _ in range(tam)]
 
-#Colocamos los barcos dentro del tablero oculto y asi descubrirlos
+#Colocamos los barcos dentro del tablero oculto aleatoriamente y asi descubrirlos
 colocar_barcos(tablero_oculto, flota)
 
 #----------------
 #Bucle principal
 #----------------
+#Mientras quedan barcos por hundir en el tablero oculto...
+while quedan_barcos(tablero_oculto):
 
-while faltan_barcos(tablero_oculto):
+    #El tablero visible sigue siendo el unico que se muestra
     mostrarTablero(tablero_visible)
 
     try:
-        fila = int(input(f"Fila: (0, {tam-1})"))
-        columna = int(input(f"Columna: (0, {tam-1})"))
+        fila = int(input(f" Introduce la primera coordenada:(0, {tam-1})"))
+        columna = int(input(f"Introduce la primera coordenada: Columna: (0, {tam-1})"))
     except ValueError:
         print("Entrada inválida, escoge otra.")
         continue
 
     if fila < 0 or fila >= tam or columna < 0 or columna >= tam:
         print("Fuera del tablero")
-    
+        continue
     #Realizamos el disparo para actualizar el tablero visible
 
     disparar(tablero_visible, tablero_oculto, fila, columna)
@@ -49,5 +58,13 @@ while faltan_barcos(tablero_oculto):
 #Final del juego
 #------------------
 
-mostrarTablero(tablero_visible)
 print("Enhorabuena,hundiste toda la flota")
+print("Tablero completo: ")
+for i in range(tam):
+    for j in range(tam):
+        if tablero_oculto[i][j] == " ":
+            tablero_visible[i][j] = Agua
+        elif tablero_oculto[i][j] in ["P","B","S","C","L"]:
+            tablero_visible[i][j] = Hundido
+            
+mostrarTablero(tablero_visible)
